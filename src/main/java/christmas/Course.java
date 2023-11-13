2166b17b8dd264d3149bd98678476a0e26e415a3
Haerang <jbu418@naver.com>
2023-11-14 05:16:06 +0900

    코스 분류 기능 추가
    주문을 받으면 해당 주문의 각 코스의 요리가 몇개인지 저장해주는 기능

---
+++ src/main/java/christmas/Course.java
@@ -0,0 +1,8 @@
+package christmas;
+
+public enum Course {
+    에피타이저,
+    메인,
+    디저트,
+    음료;
+}
--- src/main/java/christmas/Order.java
+++ src/main/java/christmas/Order.java
@@ -3,13 +3,14 @@ package christmas;
 public class Order {
     String menus[];
     String menuName[];
-    String menuNum[];
-    int totalPrice;
+    int menuNum[];
+    int courseNum[];
 
     Order(String input){
         menus = input.split(",|-");
         menuName = nameExtraction(menus);
         menuNum = numExtraction(menus);
+        courseExtraction();
     }
 
     String[] nameExtraction(String[] menus){
@@ -22,14 +23,36 @@ public class Order {
         return names;
     }
 
-    String[] numExtraction(String[] menus){
-        String[] nums = new String[menus.length / 2];
+    int[] numExtraction(String[] menus){
+        int[] nums = new int[menus.length / 2];
 
         for(int i = 1; i < menus.length; i = i + 2) {
-            nums[i / 2] = menus[i];
+            nums[i / 2] = Integer.parseInt(menus[i]);
         }
 
         return nums;
     }
 
+    void courseExtraction(){
+        Menu nameOfMenu;
+        Course nameOfCourse;
+        int index;
+
+        initMenuCourse();
+
+        for(int i = 0; i < menuName.length; i++) {
+            nameOfMenu = Menu.valueOf(menuName[i]);
+            nameOfCourse = Course.valueOf(nameOfMenu.getCourse());
+            index = nameOfCourse.ordinal();
+            courseNum[index] = courseNum[index] + menuNum[i];
+        }
+    }
+
+    void initMenuCourse(){
+        courseNum = new int[Course.values().length];
+
+        for(int i = 0; i < courseNum.length; i++){
+            courseNum[i] = 0;
+        }
+    }
 }
